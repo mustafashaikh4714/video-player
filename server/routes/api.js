16d762269776a4e87af245296872dc8bd6c5499d
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-16 19:07:38 +0530

    add put request for update video

--- server/routes/api.js
+++ server/routes/api.js
@@ -49,4 +49,20 @@ router.post('/video', function (req, res) {
     });
 });
 
+router.put('/video/:id', function (req, res) {
+    console.log("update a video");
+    Video.findOneAndUpdate(req.params.id, {
+        $set: {title: req.body.title, url: req.body.url, description: req.body.description}
+    },
+  {
+      new: true
+  }, function (err, updatedVideo) {
+     if(err) {
+         console.log("error updating video")
+     } else {
+         res.json(updatedVideo);
+     }
+  })
+})
+
 module.exports = router;
