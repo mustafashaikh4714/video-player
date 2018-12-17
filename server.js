c86d97d247b9583972617b8fca2c2b4229513549
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-17 22:58:08 +0530

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use("/api", api);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist','index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
