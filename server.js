0e5d8109619c073b73c8d6359c9813d1f3ddf70f
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-17 16:16:05 +0530

    set port for environment

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const path = require('path');
 
 const api = require("./server/routes/api");
 
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
