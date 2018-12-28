34fd0a66c47ac9de3eac41c71c04905ed8b993f9
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-28 16:44:19 +0530

    add

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(bodyParser.urlencoded({ extended: true }))
-=
+
 app.use("/api", api)
 
 app.get("*", (req, res) => {
